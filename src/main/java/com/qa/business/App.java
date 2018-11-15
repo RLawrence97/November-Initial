3b322e2078c8a2e35daf5f05c1f98cc7fa9b797d
ross lawrence <ross.g.lawrence@gmail.com>
2018-11-15 13:55:35 +0000

    minor JAX changes

--- src/main/java/com/qa/business/App.java
+++ src/main/java/com/qa/business/App.java
@@ -3,7 +3,9 @@ package com.qa.business;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -18,24 +20,26 @@ public class App {
 	private AccountRepository repoManager;
 	
 	@GET
-	@Path("/all")
+	@Path("{all}")
 	@Produces({ "application/json" })
 	public String getAll() {
 		StringBuilder sb = new StringBuilder();
 		List<Account> all = repoManager.findAllAccount();
+		sb.append("{");
 		for (Account a: all) {
 			try {
-				sb.append(util.convertObjectToJSON(a) + "\n");
+				sb.append(util.convertObjectToJSON(a) + ", ");
 			} catch (JsonProcessingException e) {
 				e.printStackTrace();
 				sb.append("{\"attempt\" : \"failed\"}");
 			}
 		}
+		sb.append("}");
 		return sb.toString();
 	}
 	
 	@GET
-	@Path("/id")
+	@Path("{id}")
 	@Produces({ "application/json" })
 	public String getbyID(@PathParam("id") int incomingID) {
 		try {
@@ -46,15 +50,15 @@ public class App {
 		return null;
 	}
 	
-	@GET
-	@Path("/remove")
+	@DELETE
+	@Path("{remove}")
 	@Produces({ "application/json" })
 	public String delete(@PathParam("id") int incomingID) {
 		return "{\" deleted account\" : \"" +repoManager.removeAccount(incomingID) + "\"}";
 	}
 	
-	@GET
-	@Path("/add")
+	@POST
+	@Path("{add}")
 	@Produces({ "application/json" })
 	public String addAccount(@PathParam("fN") String incomingFN, @PathParam("lN") String incomingLN, @PathParam("accNo") String incomingAN) {
 		try {
